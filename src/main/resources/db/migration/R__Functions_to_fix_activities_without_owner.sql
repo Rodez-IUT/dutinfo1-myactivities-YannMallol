b90ccaa6707b24e55b311aad1b421a088c228bb6
Yann Mallol <yann.mallol@iut-rodez.fr>
2019-03-20 16:18:21 +0100

    nouvelle fonction

--- src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
+++ src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
@@ -8,7 +8,7 @@ create or replace function get_default_owner() returns "user" as $$
 		select * into defaultOwner from "user" 
 		    where username = defaultOwnerUsername;
 		if not found then
-	    	insert into "user" (id,username) 
+	    	insert into "user" (id, username) 
 	    	values (nextval('id_generator'),defaultOwnerUsername); 
 	    Select * into defaultOwner from "user"
 	        where username = defaultOwnerUsername;
@@ -17,3 +17,29 @@ create or replace function get_default_owner() returns "user" as $$
 	
 	END
 $$ language plpgsql;
+
+
+/* 1) cherhcher les activtes sans "owner"
+ * Attribuer à ses activités le "defaultOwner";
+ * Retourner les activités modifiées
+ */	
+ 
+ 
+ 
+ 
+ create or replace function fix_activities_without_owner() returns setof activity as $$
+	DECLARE
+	    defaultOwner "user"%rowtype;
+		nowDate date = now();                                              
+	BEGIN
+		defaultOwner := get_default_owner();
+		return query
+		    update activity
+		    set owner_id = defaultOwner.id,
+		    modification_date = nowDate
+		where owner_id IS NULL
+		returning *;
+	    
+	END
+$$ language plpgsql;
+	
